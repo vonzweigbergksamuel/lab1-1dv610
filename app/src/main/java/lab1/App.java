81d09c8d3f589d2eb52662690588c7eede39921c
Samuel von Zweigbergk <sv222rr@student.lnu.se>
2024-09-03 13:33:04 +0200

    first commit

---
+++ app/src/main/java/lab1/App.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class App {
+    public static void main(String[] args) {
+        MorseGenerator morseGenerator = new MorseGenerator("Hejsan jag heter Samuel");
+        morseGenerator.getMessage();
+    }
+}
---
+++ app/src/main/java/lab1/MorseGenerator.java
@@ -0,0 +1,112 @@
+package lab1;
+
+import java.util.ArrayList;
+
+public class MorseGenerator {
+  private String message;
+  private ArrayList<String> messageInMorse = new ArrayList<String>();
+
+  public MorseGenerator(String message) {
+    this.message = message;
+    this.message = message.toLowerCase();
+  }
+
+  public void getMessage() {
+    for (int i = 0; i < this.message.length(); i++) {
+      this.messageInMorse.add(this.convertToMorse(this.message.charAt(i)));
+    }
+
+    for (String morse : this.messageInMorse) {
+      if (morse != this.messageInMorse.get(this.messageInMorse.size() - 1))
+        System.out.print(morse + " ");
+      else
+        System.out.print(morse);
+    }
+  }
+
+  private String convertToMorse(char i) {
+    String morse = "";
+    switch (i) {
+      case 'a':
+        morse = ".-";
+        break;
+      case 'b':
+        morse = "-...";
+        break;
+      case 'c':
+        morse = "-.-.";
+        break;
+      case 'd':
+        morse = "-..";
+        break;
+      case 'e':
+        morse = ".";
+        break;
+      case 'f':
+        morse = "..-.";
+        break;
+      case 'g':
+        morse = "--.";
+        break;
+      case 'h':
+        morse = "....";
+        break;
+      case 'i':
+        morse = "..";
+        break;
+      case 'j':
+        morse = ".---";
+        break;
+      case 'k':
+        morse = "-.-";
+        break;
+      case 'l':
+        morse = ".-..";
+        break;
+      case 'm':
+        morse = "--";
+        break;
+      case 'n':
+        morse = "-.";
+        break;
+      case 'o':
+        morse = "---";
+        break;
+      case 'p':
+        morse = ".--.";
+        break;
+      case 'q':
+        morse = "--.-";
+        break;
+      case 'r':
+        morse = ".-.";
+        break;
+      case 's':
+        morse = "...";
+        break;
+      case 't':
+        morse = "-";
+        break;
+      case 'u':
+        morse = "..-";
+        break;
+      case 'v':
+        morse = "...-";
+        break;
+      case 'w':
+        morse = ".--";
+        break;
+      case 'x':
+        morse = "-..-";
+        break;
+      case 'y':
+        morse = "-.--";
+        break;
+      case 'z':
+        morse = "--..";
+        break;
+    }
+
+    return morse;
+  }
+}
