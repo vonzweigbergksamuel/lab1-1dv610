87c95269e2434533f98bff02bc8c5c6365e5b82b
Samuel von Zweigbergk <sv222rr@student.lnu.se>
2024-09-09 08:49:23 +0200

    Cleanup files

--- app/src/main/java/lab1/Window.java
+++ app/src/main/java/lab1/Window.java
@@ -1,22 +1,54 @@
 package lab1;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
 public class Window {
 
   private JFrame frame;
+  private JPanel panel;
+  private JTextField textField;
+  private JLabel label;
 
   public Window() {
-    System.out.println("Window created");
+    frame = new JFrame("Morse Code Translator");
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.setSize(400, 400);
+    frame.setLocationRelativeTo(null);
+    frame.setLayout(new BorderLayout(10, 10));
 
-    this.frame = new JFrame("Morse Code Translator");
-    this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.frame.setSize(400, 400);
-    this.frame.setLocationRelativeTo(null);
+    panel = new JPanel();
+    panel.setLayout(new FlowLayout());
+    panel.add(createLabel());
+    panel.add(createTextField());
 
+    frame.add(panel, BorderLayout.CENTER);
   }
 
   public void showFrame() {
-    this.frame.setVisible(true);
+    frame.setVisible(true);
+  }
+
+  private JTextField createTextField() {
+    textField = new JTextField(20);
+
+    return textField;
+  }
+
+  private JLabel createLabel() {
+    label = new JLabel("Morse Code: ");
+    label.setFont(new Font("Arial", Font.PLAIN, 20));
+
+    label.setHorizontalAlignment(SwingConstants.CENTER);
+    label.setBounds(0, 0, 500, 100);
+
+    return label;
   }
 }
